772d498681611b7b40669585964e798b91312566
Rubik <hybriddodod@gmail.com>
2020-12-17 01:44:02 +0300

    pretty print and docs

--- src/ru/vsu/mpi/Main.java
+++ src/ru/vsu/mpi/Main.java
@@ -7,48 +7,40 @@ import mpi.ShiftParms;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * Задача 6, пункт 2
+ * Даны две матрицы размером n * n (n - количество процессов), матрицы распределены между процессами
+ * Каждый процесс генерирует строку A и строку (столбец) B
+ * Для вычисления по формуле процесс использует свою строку A, а строки (столбцы) B передаются
+ * последовательно каждому процессу, при этом используется топология кольцо
+ * res(i) = max(j) sum(k) ( a(i,k) + b(k,j) )
+ *
+ * Запуск: mpjrun.sh -np 3 ru.vsu.mpi.Main
+ */
 public class Main {
 
     public static final int MASTER = 0;
+
 
     public static void main(String[] args) {
         try {
             MPI.Init(args);
             int rank = MPI.COMM_WORLD.Rank();
             int size = MPI.COMM_WORLD.Size();
+            // формирование строки матрицы A и столбца B
             int[] aStr = randVector(size);
             int[] bCol = randVector(size);
-            int[] aRecvBuf = null;
-            int[] bRecvBuf = null;
-            if (rank == MASTER) {
-                aRecvBuf = new int[size * size + size];
-                bRecvBuf = new int[size * size + size];
-            }
-            int[] aSendBuf = formSendBuf(aStr, rank);
-            int[] bSendBuf = formSendBuf(bCol, rank);
-            int[] sendCount = new int[size];
-            Arrays.fill(sendCount, size + 1);
-            MPI.COMM_WORLD.Gatherv(
-                    aSendBuf, 0, size + 1, MPI.INT,
-                    aRecvBuf, 0, sendCount, createBlocksFromSendCount(sendCount), MPI.INT, MASTER
-            );
-            MPI.COMM_WORLD.Gatherv(
-                    bSendBuf, 0, size + 1, MPI.INT,
-                    bRecvBuf, 0, sendCount, createBlocksFromSendCount(sendCount), MPI.INT, MASTER
-            );
-            if (rank == MASTER) {
-                System.out.println("Matrix A");
-                printRows(aRecvBuf, size);
-                System.out.println("Matrix B");
-                printCols(bRecvBuf, size);
-            }
 
+            formAndPrintMatrix(aStr, bCol, rank, size);
+
+            // создание топологии кольцо
             int[] dims = new int[] {size};
             boolean[] periods = new boolean[] {true};
             Cartcomm cartcomm = MPI.COMM_WORLD.Create_cart(dims, periods, false);
             int[] coords = cartcomm.Coords(rank);
             ShiftParms shift = cartcomm.Shift(0, -1);
             int max = Integer.MIN_VALUE;
+            // для каждого процесса посчитать сумму, обновить максимум и обменять столбец B
             for (int k = 0; k < size; ++k) {
                 int sum = 0;
                 for (int j = 0; j < size; ++j) {
@@ -59,7 +51,7 @@ public class Main {
                 }
                 cartcomm.Sendrecv_replace(bCol, 0, bCol.length, MPI.INT, shift.rank_dest, 0, shift.rank_source, 0);
             }
-            System.out.println("proc " + rank + ": " + max);
+            System.out.println("Process " + Arrays.toString(coords) + ": " + max);
         } finally {
             MPI.Finalize();
         }
@@ -69,9 +61,36 @@ public class Main {
         Random random = new Random();
         int[] vector = new int[size];
         for (int i = 0; i < size; ++i) {
-            vector[i] = (random.nextInt(10) + 1);
+            vector[i] = (random.nextInt(10));
         }
         return vector;
+    }
+
+    private static void formAndPrintMatrix(int[] aStr, int[] bCol, int rank, int size) {
+        int[] aRecvBuf = null;
+        int[] bRecvBuf = null;
+        if (rank == MASTER) {
+            aRecvBuf = new int[size * size + size];
+            bRecvBuf = new int[size * size + size];
+        }
+        int[] aSendBuf = formSendBuf(aStr, rank);
+        int[] bSendBuf = formSendBuf(bCol, rank);
+        int[] sendCount = new int[size];
+        Arrays.fill(sendCount, size + 1);
+        MPI.COMM_WORLD.Gatherv(
+                aSendBuf, 0, size + 1, MPI.INT,
+                aRecvBuf, 0, sendCount, createBlocksFromSendCount(sendCount), MPI.INT, MASTER
+        );
+        MPI.COMM_WORLD.Gatherv(
+                bSendBuf, 0, size + 1, MPI.INT,
+                bRecvBuf, 0, sendCount, createBlocksFromSendCount(sendCount), MPI.INT, MASTER
+        );
+        if (rank == MASTER) {
+            System.out.println("Matrix A");
+            printRows(aRecvBuf, size);
+            System.out.println("Matrix B");
+            printCols(bRecvBuf, size);
+        }
     }
 
     private static void printRows(int[] buf, int size) {
@@ -82,8 +101,10 @@ public class Main {
                     .append(" [");
             for (int j = 1; j < (size + 1); ++j) {
                 stringBuilder
-                        .append(buf[offset + j])
-                        .append(", ");
+                        .append(buf[offset + j]);
+                if (j < size) {
+                    stringBuilder.append(", ");
+                }
             }
             stringBuilder.append("]");
             System.out.println(stringBuilder.toString());
@@ -102,8 +123,10 @@ public class Main {
             stringBuilder = new StringBuilder();
             stringBuilder.append("[");
             for (int j = 0; j < size; ++j) {
-                stringBuilder.append(buf[(size + 1) * j + i])
-                        .append(", ");
+                stringBuilder.append(buf[(size + 1) * j + i]);
+                if (j < size - 1) {
+                    stringBuilder.append(", ");
+                }
             }
             stringBuilder.append("]");
             System.out.println(stringBuilder);
